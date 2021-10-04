9493d50a9904870b90cbca18507eab0386003ab9
hj6hk <hj6hki123@gmail.com>
2021-10-05 00:11:18 +0800

    pageview2

--- app/src/main/java/com/example/newtips/page1.java
+++ app/src/main/java/com/example/newtips/page1.java
@@ -57,6 +57,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -83,7 +84,7 @@ public class page1 extends Fragment {
     Set<String> sett;
 
 //TODO:UI 宣告
-    Spinner spinner;
+    Spinner spinner,spinner_guage1,spinner_guage2;
     GaugeView gaugeView_Vupp,gaugeView_Vdown,gaugeView_Iupp,gaugeView_Idown;
     TextView textView_VOLT1,textView_CURRENT1,textView_WATT1,textView_FREQ1,textView_KHW1,textView_PF1;
     TextView textView_VOLT2,textView_CURRENT2,textView_WATT2,textView_FREQ2,textView_KHW2,textView_PF2;
@@ -109,7 +110,7 @@ public class page1 extends Fragment {
         gaugeView_Iupp = (GaugeView) root.findViewById(R.id.gauge_view_Iupp);
         gaugeView_Iupp.setShowRangeValues(true);
         gaugeView_Iupp.setTargetValue(0);
-        gaugeView_Iupp.setAlpha((float)0.1);
+        gaugeView_Iupp.setAlpha((float)0);
 
         gaugeView_Vdown = (GaugeView) root.findViewById(R.id.gauge_view_Vdown);
         gaugeView_Vdown.setShowRangeValues(true);
@@ -119,7 +120,7 @@ public class page1 extends Fragment {
         gaugeView_Idown = (GaugeView) root.findViewById(R.id.gauge_view_Idown);
         gaugeView_Idown.setShowRangeValues(true);
         gaugeView_Idown.setTargetValue(0);
-        gaugeView_Idown.setAlpha((float)0.1);
+        gaugeView_Idown.setAlpha((float)0);
 
         textView_VOLT1 = (TextView) root.findViewById(R.id.textViewVOLT1);
         textView_CURRENT1 = (TextView)root.findViewById(R.id.textViewCURRENT1);
@@ -136,7 +137,8 @@ public class page1 extends Fragment {
         textView_PF2 = (TextView)root.findViewById(R.id.textViewPF2);
 
         spinner=(Spinner) root.findViewById(R.id.spinner2);
-
+        spinner_guage1=(Spinner) root.findViewById(R.id.spinner_gauge1);
+        spinner_guage2=(Spinner) root.findViewById(R.id.spinner_gauge2);
     }
 
     @Override
@@ -144,6 +146,7 @@ public class page1 extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         pref =getActivity().getPreferences(Context.MODE_PRIVATE);
         initUI(view);//UI初始化
+        //macaddress_spinner初始化與事件
         spinnerAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
@@ -163,6 +166,67 @@ public class page1 extends Fragment {
 
             }
         });
+        //guage_spinner初始化與事件
+        ArrayAdapter<CharSequence> adapter_guage =
+                ArrayAdapter.createFromResource(getActivity(),    //對應的Context
+                        R.array.gauge_array,                             //資料選項內容
+                        android.R.layout.simple_spinner_item);  //預設Spinner未展開時的View(預設及選取後樣式)
+        adapter_guage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_guage1.setAdapter(adapter_guage);
+        spinner_guage2.setAdapter(adapter_guage);
+        spinner_guage1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String sPos=String.valueOf(position);
+                String sInfo=parent.getItemAtPosition(position).toString();
+                switch (sInfo)
+                {
+                    case "電壓表":
+                        gaugeView_Vupp.setAlpha((float)1);
+                        gaugeView_Iupp.setAlpha((float)0);
+                        break;
+                    case"電流表":
+                        gaugeView_Vupp.setAlpha((float)0);
+                        gaugeView_Iupp.setAlpha((float)1);
+                        break;
+                    default:
+                        break;
+                }
+                Toast.makeText(getActivity(),sInfo,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner_guage2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String sPos=String.valueOf(position);
+                String sInfo=parent.getItemAtPosition(position).toString();
+                switch (sInfo)
+                {
+                    case "電壓表":
+                        gaugeView_Vdown.setAlpha((float)1);
+                        gaugeView_Idown.setAlpha((float)0);
+                        break;
+                    case"電流表":
+                        gaugeView_Vdown.setAlpha((float)0);
+                        gaugeView_Idown.setAlpha((float)1);
+                        break;
+                    default:
+                        break;
+                }
+                Toast.makeText(getActivity(),sInfo,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         switch1=view.findViewById(R.id.switch1);
         switch2=view.findViewById(R.id.switch2);
@@ -243,6 +307,14 @@ public class page1 extends Fragment {
                             @Override
                             public void run() {
                                 //TODO:updata UI
+
+                                if(Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Volt1")))<=230 && Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Volt2")))<=230 && Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Current1")))<=16&& Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Current2")))<=16)
+                                {
+                                    gaugeView_Vupp.setTargetValue(Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Volt1"))));
+                                    gaugeView_Iupp.setTargetValue(Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Current1"))));
+                                    gaugeView_Vdown.setTargetValue(Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Volt2"))));
+                                    gaugeView_Idown.setTargetValue(Float.parseFloat(Objects.requireNonNull(GlobalData.datamap_getserver.get("Current2"))));
+                                }
                                 textView_VOLT1.setText(GlobalData.datamap_getserver.get("Volt1"));
                                 textView_CURRENT1.setText(GlobalData.datamap_getserver.get("Current1"));
                                 textView_WATT1.setText(GlobalData.datamap_getserver.get("Watt1"));
